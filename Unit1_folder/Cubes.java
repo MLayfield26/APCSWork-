f95f9125c0342d025d99cd8220c46bea6c1c621f
MLayfield26 <112432448+MLayfield26@users.noreply.github.com>
2022-09-30 10:39:21 -0700

    UploadTest2

---
+++ Unit1_folder/Cubes.java
@@ -0,0 +1,18 @@
+public class Cubes
+{
+  public static void main(String args[])
+  {
+  int a=1;
+  int b =1;
+
+for(int i=0; i < 10; i++){
+//  a =(a-1)+(b+2);
+
+System.out.println(a);
+b = b+1;
+a = b*b;
+
+
+}
+  }
+}
---
+++ Unit1_folder/Geometric.java
@@ -0,0 +1,17 @@
+public class Geometric
+{
+  public static void main(String args[])
+  {
+  int a=3;
+
+
+for(int i=0; i < 10; i++){
+//  a =(a-1)+(b+2);
+
+System.out.println(a);
+a=a*2;
+
+
+}
+  }
+}
