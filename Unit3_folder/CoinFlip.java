5c6f3665509f049079ab869e21de7dc61dccdc78
MLayfield26 <112432448+MLayfield26@users.noreply.github.com>
2022-10-06 13:04:03 -0700

    UPLOADFOLDER3

---
+++ Unit3_folder/CoinFlip.java
@@ -0,0 +1,11 @@
+public class JavaFlip {
+        public static void main(String[] args) {
+         for (int i = 0; i < 10; i++) {
+                if (Math.random() < 0.5){
+                        System.out.println("Heads");
+                }else{
+                        System.out.println("Tails");
+                }
+        }
+      }
+}
---
+++ Unit3_folder/MonkeyTrouble.java
@@ -0,0 +1,14 @@
+public class MonkeyTrouble{
+      public static void main(String[] args) {
+int aSmile1 = 1;
+int bSmile1 = 6;
+
+
+if(aSmile1 == bSmile1)
+System.out.println("TROUBLE");
+
+else
+System.out.println("no trouble!");
+
+      }
+}
