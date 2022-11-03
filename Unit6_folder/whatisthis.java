00266c5ed9b2f2e6964228213020c721b78c30d7
MLayfield26 <112432448+MLayfield26@users.noreply.github.com>
2022-11-03 13:08:31 -0700

    jjj

---
+++ Unit6_folder/whatisthis.java
@@ -0,0 +1,62 @@
+//
+// Bounce.java
+//
+
+
+import processing.core.*;
+import java.util.*;
+
+
+public class whatisthis extends PApplet
+{
+    public void settings()
+    {
+        size(800, 800);
+    }
+
+    public void setup()
+    {
+        balls = new ArrayList<Ball>();
+        createBall();
+    }
+
+    public void createBall()
+    {
+        PVector position = new PVector(mouseX,mouseY);
+
+        PVector velocity = PVector.random2D();
+        velocity.setMag(random(100));
+
+        float radius = random(20);
+        int c = color(random(256), random(256), random(256));
+
+        Ball ball = new Ball(this,
+                             position,
+                             velocity,
+                             radius,
+                             c);
+
+        balls.add(ball);
+    }
+
+    public void draw()
+    {
+        background(255);
+
+        for (Ball ball : balls)
+            ball.display();
+    }
+
+    public void keyPressed()
+    {
+        for (int i=0; i<1; i++)
+            createBall();
+    }
+
+    public static void main(String[] args)
+    {
+        PApplet.main("Project2");
+    }
+
+    private ArrayList<Ball> balls;
+}
