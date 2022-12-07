9279050f89dec8e9d5a76bb9ce297074050dd7c4
MLayfield26 <112432448+MLayfield26@users.noreply.github.com>
2022-12-07 12:36:04 -0800

    ww

--- Unit7_folder/Unit7.java
+++ Unit7_folder/Unit7.java
@@ -1,43 +1,47 @@
 //Sum of squares (array)
 //Write a function that takes a single array of doubles as input, and returns the sum of squares of the values.
-
+import java.util.*;
 public class Unit7{
 
-//dear future me pls make this 
-  for (int i=0; i<names.length; i++)
-  {
-      String name = names[i];
-      System.out.println(name);
+public static void main(String[] args){
+ ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(21);
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(7);
+for(int n : numbers){
+if (n % 7 == 0) {
+ System.out.println(n + " is lucky");
+}
+else
+System.out.println(n + " isnt lucky");
+}
+
+int Squares[] = {1, 2, 3, 4, 5};
+int sum = 0;
+
+for (int i : Squares){
+    sum += i * i;
+}
+  System.out.println("some of squares: " + sum);
+
+
+String[] names = {"zebra", "ziptie", "zipuZ"};
+ for (int b=0; b<names.length; b++){
+      String name = names[b];
+//      System.out.println(name);
       int length = name.length();
       int count = 0;
+      for(int i = 0; i < name.length(); i++){
         if (name.charAt(i) == 'Z' || name.charAt(i) == 'z'){
-      if (i < length-1 && !Character.isLetter(name.charAt(i+1)))
-            count++;
-        else if (i == length-1)
-            count++;
+            count+=1;
+      }
 
       }
-System.out.println("number of Zs: " + count);
-  }
+    System.out.println(name + " number of Zs: " + count);
+}
+        }
 
 
-  System.out.println();
 
-  // for-each loop
-
-  for (String name : names)
-  {
-      System.out.println(name);
-  }
-
-public static void main(String[] args){
-  String[] names = {"zebra", "ziptie", "zipuZ"};
-
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-
-
-        System.out.println();
-
-  }
 }
