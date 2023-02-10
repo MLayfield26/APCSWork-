07953b211ef8715ad30beb67c6f08fc08e50c0cf
MLayfield26 <112432448+MLayfield26@users.noreply.github.com>
2023-02-10 11:47:58 -0800

    commit to main

---
+++ Unit11_folder/Car.java
@@ -0,0 +1,28 @@
+//
+// Rectangle.java
+//
+
+
+public class Rectangle implements vehicle
+{
+    public Rectangle(double w, double h)
+    {
+        this.w = w;
+        this.h = h;
+    }
+
+    public String name() {return "Rectangle";}
+
+    public int sideCount()
+    {
+        return 4;
+    }
+
+    public double perimeter()
+    {
+        return 2*w + 2*h;
+    }
+
+    private double w;
+    private double h;
+}
---
+++ Unit11_folder/vehicleStats.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class vehicleStats{
+
+public static int totalWheels(Arraylist<Vehicle>vehicles){
+  int total = 0;
+
+  for(vehicle v: vehicles){
+    total += v.wheelCount();
+  }
+  return total;
+}
+public static boolean isHumanPowered(Arraylist<Vehicle>vehicles){
+  for(vehicle v: vehicles){
+    if(boolean == true){
+
+    }
+
+
+}
+
+pub]lic static void Main(String[] args){
+  Vehicle electricScooter = new Vehicle("ElectricScooter");
+       System.out.println(drkessner.hello());
+
+       Car towTruck = new Car("Tow Truck");
+      // System.out.println(sarah.hello());
+         System.out.println();
+
+       ArrayList<Vehicle> vehicle = new ArrayList<Vehicle>();
+       vehicle.add(electricScooter);
+       vehicle.add(towTruck);
+
+       for (Vehicle v : vehicle)
+        //   System.out.println(p.hello());
+
+}
+}
